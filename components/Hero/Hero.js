090252f57e0748623dd46a011a12d0301042cee4
daneburns <daneb91@gmail.com>
2021-08-08 09:04:01 -0700

    added a few owl animations

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,13 +1,14 @@
 import { useState, useRef, useEffect, Suspense } from "react";
 import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { useSpring, animated } from "@react-spring/three";
 import { GLTFLoader } from "three-stdlib";
 import { OBJLoader } from "three-stdlib";
+import Owl from "../Owl.js";
 
 const GLTF = ({ url, ...props }) => {
-  const gltf = useLoader(GLTFLoader, url);
-  const mesh = useRef();
-  return <primitive ref={mesh} object={gltf.scene} />;
+  // const gltf = useLoader(GLTFLoader, url);
+  // const mesh = useRef();
+  // console.log(gltf);
+  // return <primitive ref={mesh} object={gltf.scene} />;
 };
 
 const OBJ = ({ url, ...props }) => {
@@ -17,10 +18,8 @@ const OBJ = ({ url, ...props }) => {
 };
 
 function Hero() {
-  const [rotation, api] = useSpring(() => ({ y: 1 }));
-
   return (
-    <div className="relative flex flex-row px-32 py-12 max-w-screen-2xl mx-auto">
+    <div className="relative flex flex-row  px-12 py-48">
       <div className="flex flex-1 flex-col">
         <div className="text-7xl leading-relaxed font-serif">
           <div>Arcadia Literacy & Learning</div>
@@ -32,28 +31,24 @@ function Hero() {
           <Suspense fallback={null}>
             <ambientLight intensity={0.5} />
             <spotLight intensity={0.5} position={[0, -1, 3]} />
-
             <pointLight
               position={[0, 2, 3]}
               intensity={0.8}
               color="teal"
               castShadow
             />
-            <animated.mesh
-              position={[0, -1, 3]}
-              rotation={[0, 0, 0]}
-              receiveShadow
-            >
+            {/* <mesh position={[0.5, -1, 3]} rotation={[0, -0.5, 0]} receiveShadow>
               <GLTF url={"./Owl.gltf"} />
-            </animated.mesh>
+            </mesh> */}
             <mesh
-              position={[0, -1, 1]}
+              position={[0.75, -1, 1]}
               rotation={[0, 0, 0]}
               scale={0.4}
               receiveShadow
             >
               <OBJ url={"./books.obj"} />
             </mesh>
+            <Owl position={[0, -1, 3]} />
           </Suspense>
         </Canvas>
       </div>
--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -5,9 +5,9 @@ import Link from "next/link";
 
 export default function Nav() {
   return (
-    <nav className="bg-brown-500 text-cloud-500 z-20">
+    <nav className=" text-cloud-800 z-20">
       <div className="flex flex-row justify-start px-10 py-4 items-center">
-        <div className="h-auto w-48 hover:text-white">
+        <div className="h-auto w-48 hover:text-brown-500 text-brown-600">
           <Link href="/">
             <a href="/">
               <Logo variant={true} />
